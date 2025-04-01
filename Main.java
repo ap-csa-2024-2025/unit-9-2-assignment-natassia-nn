e1d9c666777434911e404a608aaf3baca5662529
natassia-nn <natassianguyen@gmail.com>
2025-04-01 11:42:08 -0700

    made changes

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main
 {
   public static void main(String[] args)
   {
-
+    Coffee c = new Coffee("large", true, 10, "latte");
+    SpecialityCoffee sp = new SpecialityCoffee("medium", false, 30, "machiatto", "matcha");
+ 
+    System.out.println(c); // implicit call to c.toString()
+    System.out.println(sp);
     // Uncomment to test the Car classes
     // CarTester.run();
 
--- SpecialtyCoffee.java
+++ SpecialtyCoffee.java
@@ -24,20 +24,20 @@ public class SpecialtyCoffee extends Coffee
   public double getPrice()
   {
     // we will edit in class
-    return 0.0;
+    return (super.getPrice() * 1.5);
   }
 
   // Method override of Coffee toString
   public String toString()
   {
-    // we will edit in class
-    return null;
+    return super.toString() + "with" + flavor;
   }
 
   // print a message that compares the prices of a regular Coffee
   // and a SpecialtyCoffee using both getPrice methods
   public void printPriceComparison()
   {
-
+      System.out.println("A regular coffee costs " + super.getPrice());
+      System.out.println("A speciality coffee costs " + this.getPrice());
   }
 }
